746f447c728c30989cfac889e3743fb5ddb78b4f
nnn1k <nikitos.elidu@gmail.com>
2023-04-05 02:11:18 +0300

    t

---
+++ task_stack.py
@@ -0,0 +1,45 @@
+class my_stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, el):
+        self.stack.append(el)
+
+    def pop(self):
+        return self.stack.pop(-1)
+
+    def size(self):
+        return len(self.stack)
+
+def test_1():
+    s = my_stack()
+    s.push(1)
+    assert s.pop() == 1
+
+
+test_1()
+
+
+def test_2():
+    s = my_stack()
+    s.push(1)
+    assert s.size() == 1
+
+
+test_2()
+
+
+def test_3():
+    s = my_stack()
+    s.push(1)
+    s.push(2)
+    s.pop()
+    s.push(3)
+    s.push(4)
+    assert s.pop() == 4
+    assert s.size() == 2
+    assert s.pop() == 3
+
+
+test_3()
